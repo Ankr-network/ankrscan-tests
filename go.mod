0224822a1d78cf34ed7772f9353c4da4be6f104d
khasan <xozzslip@gmail.com>
2022-02-17 12:57:46 +0300

    add test entitites

---
+++ go.mod
@@ -0,0 +1,24 @@
+module ankrscan-tests
+
+go 1.17
+
+require (
+	github.com/Ankr-network/ankrscan-proto-contract v0.0.0-20220216120246-c0b037bf4b00
+	github.com/ethereum/go-ethereum v1.10.16
+	github.com/golang/protobuf v1.5.2
+	github.com/stretchr/testify v1.7.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/genproto v0.0.0-20220204002441-d6cc3cc0770e // indirect
+	google.golang.org/grpc v1.44.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
---
+++ testentity/entity.go
@@ -0,0 +1,165 @@
+package testentity
+
+import "testing"
+
+import (
+	"bytes"
+	"github.com/Ankr-network/ankrscan-proto-contract/go/proto"
+	"github.com/ethereum/go-ethereum/common"
+	proto2 "github.com/golang/protobuf/proto"
+	"github.com/stretchr/testify/require"
+	"math/rand"
+	"strings"
+)
+
+func ProtoEqual(t *testing.T, message1 proto2.Message, message2 proto2.Message) {
+	buf1 := proto2.NewBuffer([]byte{})
+	require.NoError(t, buf1.EncodeMessage(message1))
+	buf2 := proto2.NewBuffer([]byte{})
+	require.NoError(t, buf2.EncodeMessage(message2))
+	require.Equal(t, len(buf1.Bytes()), len(buf2.Bytes()))
+	require.Equal(t, 0, bytes.Compare(buf1.Bytes(), buf2.Bytes()))
+}
+
+func GenAddress() []byte {
+	token := make([]byte, 20)
+	rand.Read(token)
+	return token
+}
+
+func GenHash() []byte {
+	token := make([]byte, 32)
+	rand.Read(token)
+	return token
+}
+
+func GenBlockHeight() uint64 {
+	return uint64(rand.Uint32())
+}
+
+func GenBytes(size int) []byte {
+	token := make([]byte, size)
+	rand.Read(token)
+	return token
+}
+
+func GetSequenceParent(blocks []*proto.Block) []byte {
+	return blocks[len(blocks)-1].Header.BlockHash
+}
+
+func GenSequence(blocksCount int, blockchainName string, blockHeight uint64, blockHash []byte, parentHash []byte, txsCount int) []*proto.Block {
+	blocks := make([]*proto.Block, 0)
+	for i := 0; i < blocksCount; i++ {
+		blocks = append(blocks, GenBlockWithTxs(blockchainName, blockHeight+uint64(i), blockHash, parentHash, txsCount))
+		parentHash = blockHash
+		blockHash = GenHash()
+	}
+	return blocks
+}
+
+func GenBlockWithTxs(blockchainName string, blockHeight uint64, blockHash []byte, parentHash []byte, txsCount int) *proto.Block {
+	transactions := make([]*proto.Transaction, 0)
+	for i := 0; i < txsCount; i++ {
+		transactions = append(transactions, GenTx(blockchainName, blockHeight, blockHash, GenHash(), uint64(i), 500, 10, 40))
+	}
+	return GenBlock(blockchainName, blockHeight, blockHash, parentHash, transactions)
+}
+
+func GenBlockWithLargeTxs(blockchainName string, blockHeight uint64, blockHash []byte, txsCount int, inputSize int) *proto.Block {
+	transactions := make([]*proto.Transaction, 0)
+	for i := 0; i < txsCount; i++ {
+		transactions = append(transactions, GenTx(blockchainName, blockHeight, blockHash, GenHash(), uint64(i), inputSize, 10, 40))
+	}
+	return GenBlock(blockchainName, blockHeight, blockHash, GenHash(), transactions)
+}
+
+func GenBlock(blockchainName string, blockHeight uint64, blockHash []byte, parentHash []byte, transactions []*proto.Transaction) *proto.Block {
+	block := &proto.Block{
+		Header: &proto.BlockHeader{
+			BlockchainName:    blockchainName,
+			BlockHeight:       blockHeight,
+			BlockHash:         blockHash,
+			ParentHash:        parentHash,
+			Timestamp:         rand.Uint64(),
+			TransactionsCount: uint64(len(transactions)),
+			Specific: &proto.BlockHeader_EthBlock{
+				EthBlock: &proto.EthBlock{
+					Nonce:            GenAddress(),
+					Sha3Uncles:       GenHash(),
+					TransactionsRoot: GenHash(),
+					StateRoot:        GenHash(),
+					Miner:            GenAddress(),
+					Difficulty:       GenHash(),
+					TotalDifficulty:  GenHash(),
+					ExtraData:        GenHash(),
+					Size:             rand.Uint64(),
+					GasLimit:         rand.Uint64(),
+					GasUsed:          rand.Uint64(),
+					Uncles:           [][]byte{GenHash(), GenHash()},
+				},
+			},
+		},
+		Transactions: transactions,
+	}
+	return block
+}
+
+func GenTx(blockchainName string, blockHeight uint64, blockHash []byte, transactionHash []byte, transactionIndex uint64, inputSize, logsCount, logsDataSize int) *proto.Transaction {
+	logs := make([]*proto.EthLog, 0)
+	for i := 0; i < logsCount; i++ {
+		logs = append(logs, GenLog(uint32(i), logsDataSize))
+	}
+	return &proto.Transaction{
+		BlockchainName:   blockchainName,
+		TransactionHash:  transactionHash,
+		BlockHash:        blockHash,
+		BlockHeight:      blockHeight,
+		TransactionIndex: transactionIndex,
+		Timestamp:        rand.Uint64(),
+		Specific: &proto.Transaction_EthTx{
+			EthTx: &proto.EthTransaction{
+				Nonce:             rand.Uint64(),
+				From:              GenAddress(),
+				To:                GenAddress(),
+				Value:             GenHash(),
+				Gas:               rand.Uint64(),
+				GasPrice:          GenHash(),
+				Input:             GenBytes(inputSize),
+				ContractAddress:   GenAddress(),
+				CumulativeGasUsed: rand.Uint64(),
+				GasUsed:           rand.Uint64(),
+				Status:            1,
+				Logs:              logs,
+			},
+		},
+	}
+}
+
+func GenLog(logIndex uint32, dataSize int) *proto.EthLog {
+	return &proto.EthLog{
+		Address: GenAddress(),
+		Topics:  [][]byte{GenHash(), GenHash()},
+		Data:    GenBytes(dataSize),
+		Removed: false,
+	}
+}
+
+func GenBlockchainName() string {
+	letters := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, 10)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return strings.Join([]string{string(b), "chain"}, "")
+}
+
+func CheckBlocks(t *testing.T, initial []*proto.Block, result []*proto.Block) {
+	require.Equal(t, len(initial), len(result))
+	for i := 0; i < len(initial); i++ {
+		require.Equal(t, initial[i].Header.BlockchainName, result[i].Header.BlockchainName, "index %d", i)
+		require.Equal(t, int(initial[i].Header.BlockHeight), int(result[i].Header.BlockHeight), "index %d", i)
+		require.Equal(t, common.BytesToHash(initial[i].Header.BlockHash).String(), common.BytesToHash(result[i].Header.BlockHash).String(), "index %d", i)
+		ProtoEqual(t, initial[i].Header, result[i].Header)
+		ProtoEqual(t, initial[i], result[i])
+	}
+}
